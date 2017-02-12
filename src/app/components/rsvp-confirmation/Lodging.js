bdc78fa662a9570afe58936bedd668605b471fe0
Jessica Tai <jessica.tai@airbnb.com>
2017-02-12 00:52:02 -0800

    Fix link styles

--- src/app/components/rsvp-confirmation/Lodging.js
+++ src/app/components/rsvp-confirmation/Lodging.js
@@ -5,8 +5,6 @@ import sundayImage from '../../../images/rsvp/sunday.png';
 import '../../../stylesheets/components/rsvp-confirmation.css';
 
 function Calendars({ lodgingDays, requestedLodgingDays }) {
-
-  console.log('requestedLodgingDays', requestedLodgingDays);
   if (requestedLodgingDays.length > 0) {
     return (
       <div>
@@ -42,14 +40,21 @@ function Calendars({ lodgingDays, requestedLodgingDays }) {
 
   return (
     <div>
-      <p className="error">
+      <p className="error space-2">
         You are <b>not</b> requesting on-site lodging. There are many other options
         nearby such as:
       </p>
       <ul>
-        <li>Example 1: Pine lodge</li>
-        <li>Example 2:</li>
-        <li>some other place</li>
+        <li className="space-1">
+          <a className="styled-link" href="http://www.graeagle.com/accommodations.html">
+            Graeagle accommodations
+          </a>
+        </li>
+        <li className="space-1">
+          <a className="styled-link" href="http://www.ci.portola.ca.us/portola-lodging.html">
+            Portola accommodations
+          </a>
+        </li>
       </ul>
     </div>
   );
