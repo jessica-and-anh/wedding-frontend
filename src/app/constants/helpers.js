b14fea71f09b23e99e39f66a2db3992466be0d9b
Jessica Tai <jessica.tai@airbnb.com>
2017-02-16 22:12:48 -0800

    blacklist isEditing prop

--- src/app/constants/helpers.js
+++ src/app/constants/helpers.js
@@ -1,5 +1,5 @@
 // TODO move this to a utils file since these aren't constants.
-import pick from 'lodash/pick';
+import omit from 'lodash/omit';
 
 import { API_DOMAIN } from './paths';
 
@@ -15,33 +15,16 @@ export function postRsvpGroupUrl(userGroupId) {
   return apiUrl(`rsvp/${userGroupId}`);
 };
 
-const VALID_USER_GROUP_FIELDS = [
-  'address_line1',
-  'address_line2',
-  'city',
-  'code',
-  'notes',
-  'lodging_friday',
-  'lodging_saturday',
-  'lodging_sunday',
-  'state',
-  'zipcode',
-];
-
-const VALID_USER_FIELDS = [
-  'email',
-  'is_attending',
-  'diet',
-];
+const INVALID_USER_FIELDS = ['isEditing'];
 
 export function sanitizePostData({ userGroup, users }) {
-  const usersObject = users.reduce((accumulator, user, i) => {
-    accumulator[user.id] = pick(user, VALID_USER_FIELDS);
+  const usersObject = users.reduce((accumulator, user) => {
+    accumulator[user.id] = omit(user, INVALID_USER_FIELDS);
     return accumulator;
   }, {});
 
   return {
-    user_group: pick(userGroup, VALID_USER_GROUP_FIELDS),
+    user_group: userGroup,
     users: usersObject,
-  }
+  };
 }
--- src/app/constants/paths.js
+++ src/app/constants/paths.js
@@ -1,1 +1,6 @@
-export const API_DOMAIN = 'https://bao-wedding-api.herokuapp.com';
+const PROD_API_DOMAIN = 'https://bao-wedding-api.herokuapp.com';
+const DEV_API_DOMAIN = 'http://0.0.0.0:3000';
+
+// HACKITY HACK
+const IS_DEV = true;
+export const API_DOMAIN = IS_DEV ? DEV_API_DOMAIN : PROD_API_DOMAIN;
