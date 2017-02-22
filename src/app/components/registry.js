310b0f9cee743828283ebf8ac780ef4d4063b6aa
Anh Tran <atran@linkedin.com>
2017-02-21 23:23:22 -0800

    Updating registry

--- src/app/components/registry.js
+++ src/app/components/registry.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import MediaQuery from 'react-responsive';
 import '../../stylesheets/components/registry.css';
 
-import venmo from '../../images/venmo.png';
-import paypal from '../../images/paypal.png';
 import amazon from '../../images/amazon.png';
 
 class Registry extends Component {
@@ -26,8 +24,6 @@ class Registry extends Component {
             </MediaQuery>
           </div>
           <div className="registry-section payments">
-            <a className="payment-anchor" target="_blank" href="https://venmo.com/atran-wynd"><img className="payment-vendor" src={venmo} alt="Venmo" /></a>
-            <a className="payment-anchor" target="_blank" href="https://www.paypal.me/atranwynd"><img className="payment-vendor" src={paypal} alt="Paypal" /></a>
             <a className="payment-anchor" target="_blank" href="https://www.amazon.com/wedding/anh-tran-jessica-tai-chalet-view-lodge-september-2017/registry/2G6ZHB706PXGU"><img className="payment-vendor" src={amazon} alt="Amazon" /></a>
           </div>
         </div>
