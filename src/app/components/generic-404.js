c512c9357ca986f1b531af26d4c1aaa6790c0076
Anh Tran <atran@linkedin.com>
2017-02-23 00:43:13 -0800

    Adding back button on 404

--- src/app/components/generic-404.js
+++ src/app/components/generic-404.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MediaQuery from 'react-responsive';
+import RaisedButton from 'material-ui/RaisedButton';
 import '../../stylesheets/components/generic-404.css';
 
 import corgi from '../../images/corgi.gif';
@@ -11,6 +13,14 @@ class Generic404 extends Component {
         <p className="text-404">404</p>
         <p className="sub-404">Carry on, nothing going on here...</p>
         <img src={corgi} className="corgi-image" alt="Corgi swimming" />
+        <MediaQuery maxDeviceWidth={1224}>
+          <RaisedButton
+            className="back-404"
+            label="Back"
+            href="/"
+            fullWidth={true}
+          />
+        </MediaQuery>
       </div>
     );
   }
